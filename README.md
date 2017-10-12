c8fca44a1ce0bca9a6c37b81cd599d9d050edfa0
Adrian Van Sickel <adrianvansickel@gmail.com>
2017-10-12 02:21:46 -0700

    Ready to Submit

--- README.md
+++ README.md
@@ -2,19 +2,29 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+* [Rules](#rules)
+* [Dependencies](#dependencies)
 
-## Instructions
+## Rules
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Upon loading the game, a 4x4 grid of cards will appear, and the clock will start.
+The player's objective is to find every matching pair of cards. Clicking on a card
+will flip it over to reveal its image. Once two cards are flipped over, they will
+be compared. If their images are identical, the matched pair will remain
+facing up; if they differ, the cards will be flipped to face down again. Once all
+matches are found, the player will be presented with a game report card and a prompt
+to either play again or return to the completed game's screen.
 
-To get started, open `js/app.js` and start building out the app's functionality
+The player's performance is not evaluated by the time taken to win (though this
+may be considered an added challenge), but rather by the number of moves (defined
+as an attempted match of two cards) taken per the following ranking system:
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+8-14  moves  = 3 stars    'excellent!'
+15-29 moves  = 2 stars    'good'
+30-44 moves  = 1 star     'okay...'
+45-?  moves  = 0 stars    'quite bad, really'
 
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+## Dependencies
+The game is executed via 'index.html'. This file relies on 'css/app.css' and
+'js/app.js' for the game's styling and functionality, respectively. It also
+uses two external Web-based pre-fetch stylesheets (https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css and https://fonts.googleapis.com/css?family=Coda), and the jQuery library at https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js.
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-const cards = $('.deck').children();
+const CARDS = $('.deck').children();
 
 /*
  * Display the cards on the page
@@ -9,11 +9,11 @@ const cards = $('.deck').children();
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-shuffle(cards);
-const shuffledCards = [...cards];
+shuffle(CARDS);
+const SHUFFLED_CARDS = [...CARDS];
 
-for(var i=0; i<cards.length; i++) {
-  $('.card-'+i).after(shuffledCards[i]);
+for(var i=0; i<CARDS.length; i++) {
+  $('.card-'+i).after(SHUFFLED_CARDS[i]);
   $('.card-'+i).addClass('card');
 }
 
@@ -32,6 +32,14 @@ function shuffle(array) {
     return array;
 }
 
+//This code handles the onscreen timer. When the game is won, the timer is cleared in the announceWin() function below.
+let timeInSeconds = 0;
+let timerFunction = window.setInterval(setTimer, 1000);
+function setTimer() {
+  timeInSeconds++;
+  $('.time').text(timeInSeconds);
+}
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -42,65 +50,92 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
- let chosen = [];
- let clickDisabled = false;
- let movesCount = 1;
 
- function showCard(cardToShow) {
-   cardToShow.addClass('show');
- }
+ //CARD-CLICK EVENT FUNCTIONS AND GLOBALS
+let chosen = [];
+let clickDisabled = false;
+let movesCount = 0;
+let matchCount = 0;
 
- function addOpen(chosenCard) {
+function showCard(cardToShow) {
+   cardToShow.addClass('show open');
+}
+
+function addOpen(chosenCard) {
    if(chosen[0]) {
      if(chosenCard.attr('class') == chosen[0].attr('class')) {
        return;
      }
    }
    chosen.push(chosenCard);
- }
+}
 
- function setMatch() {
+function setMatch() {
    $(chosen[0]).addClass('match');
    $(chosen[1]).addClass('match');
    chosen.pop();
    chosen.pop();
+   matchCount++;
    clickDisabled = false;
- }
+}
 
- function noMatch() {
-   $(chosen[0]).removeClass('show');
-   $(chosen[1]).removeClass('show');
+function noMatch() {
+   $(chosen[0]).removeClass('show open');
+   $(chosen[1]).removeClass('show open');
    chosen.pop();
    chosen.pop();
    clickDisabled = false;
- }
+}
 
- function setMovesCount() {
-   $('.moves').text(movesCount++);
+function setMovesCount() {
+   movesCount++;
+   $('.moves').text(movesCount);
+   if($('.stars').children().length > 0 && movesCount % 15 == 0) {
+     $('.stars').children().first().remove();
+   }
+}
+
+function announceWin() {
+   window.clearInterval(timerFunction);
+   let starOrStars = "stars";
+   if($('.stars').children().length == 1) {
+     starOrStars = "star";
+   }
+
+   if(window.confirm("You won in " + movesCount + " moves and with " + timeInSeconds + " seconds on the clock. Your rank is " + $('.stars').children().length + " " + starOrStars + "! Press OK to play again, or Cancel to return to the game screen.")) {
+     window.location.reload();
+   }
  }
 
 //When a card is clicked, the following will occur
- $('.card').click(function() {
+$('.card').click(function() {
    //This prevents the player from opening additional cards until failed matches are re-hidden.
    if(clickDisabled) {return;}
 
    //The card is shown, or "flipped over".
    showCard($(this));
 
-   //The card is added to the list of open cards in the chosen[] array.
-   //Note: Within the addOpen() function the card is checked to prevent the player from adding the same card to the chosen[] array, thus eliminating the risk of matching cards with themselves.
+   //The card is added to the list of open cards in the chosen[] array. Player is prevented from trying to match a card with itself.
    addOpen($(this));
 
-   //Once two cards are opened, their child elements' class values are evaluated. If they match, setMatch adds 'match' to their classes; if not, they lose their 'show' classes. In both cases, the chosen[] array is cleared.
+   //Check the cards. Either setMatch for successful matches, or noMatch for failed matches.
    if(chosen.length == 2) {
      if(chosen[0].children().attr('class') == chosen[1].children().attr('class')) {
        setMatch();
      } else {
        clickDisabled = true;
-       //setTimeout gives the player some time to see the values of the incorrectly matched cards before being flipped back over. Within noMatch() the clickDisabled flag is reset so that the player may start checking cards again.
+       //setTimeout gives the player some time to see the values of the incorrectly matched cards before being flipped back over.
        window.setTimeout(noMatch, 500);
      }
      setMovesCount();
    }
 
- });
+   //Once all matches are found, announceWin() is called to stop the clock and format the modal.
+   if(matchCount == 8) {
+     announceWin();
+   }
+});
+
+$('.restart').click(function() {
+  window.location.reload();
+});
